9219299df3ac0367b33669db0403d6fe5d95961c
Ethan Glenn <ethan+git@eglenn.dev>
2024-05-04 17:47:05 -0600

    Added work from online class work

--- server.js
+++ server.js
@@ -6,14 +6,26 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const path = require("path")
+
+// View Engine and Templates
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("views", path.join(__dirname, "views"))
+app.set("layout", "layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
@@ -26,5 +38,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on http://${host}:${port}`)
 })
