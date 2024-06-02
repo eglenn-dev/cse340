aa4a692a3c408f02a3b8869d0c6fcfbdbac485f5
Ethan Glenn <ethan+git@eglenn.dev>
2024-06-01 19:49:56 -0600

    Got the 404 page error handling working

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const path = require("path")
+const utilities = require('./utilities/');
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require('./routes/inventoryRoute');
 
@@ -28,6 +29,25 @@ app.use(static)
 app.get("/", baseController.buildHome);
 // Inventory Route
 app.use("/inv", inventoryRoute);
+
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  });
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
