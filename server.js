af89e556bfede1fa2d005a6da48cc129d82f1df7
Ethan Glenn <ethan+git@eglenn.dev>
2024-06-01 20:06:37 -0600

    More robust error handling implemented

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set("layout", "layouts/layout")
  *************************/
 app.use(static)
 // Index Route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory Route
 app.use("/inv", inventoryRoute);
 
@@ -41,12 +41,13 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav();
   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) { message = err.message } else { message = 'Oh no! There was a crash. Maybe try a different route?' };
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   });
-})
+});
 
 /* ***********************
  * Local Server Information
--- utilities/index.js
+++ utilities/index.js
@@ -52,4 +52,6 @@ Util.buildClassificationGrid = async function (data) {
     return grid;
 }
 
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+
 module.exports = Util;
