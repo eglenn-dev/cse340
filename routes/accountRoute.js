85c550cfa33b0c155fc6b745ac8f79fb52452f0b
Ethan Glenn <ethan+git@eglenn.dev>
2024-06-15 16:05:07 -0600

    Server side data validation completed

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/index");
+const { validate } = require('../utilities/account-validation');
 
 // Assuming 'viewAccount' is the function to handle "My Account" page
 router.get("/login", accountController.buildLogin, utilities.handleErrors);
 router.get("/register", accountController.buildRegister, utilities.handleErrors);
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post(
+    "/register",
+    validate.registrationRules(),
+    validate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
