3d7f3c3ea6a0914bfa93c27ac50a895d050fd13f
Ethan Glenn <ethan+git@eglenn.dev>
2024-07-20 15:33:25 -0600

    Updated the table and added a view button column that opens the detail page

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -21,11 +21,12 @@ classificationList.addEventListener("change", function () {
 function buildInventoryList(data) {
     let inventoryDisplay = document.getElementById("inventoryDisplay");
     let dataTable = '<thead>';
-    dataTable += '<tr><th>Vehicle Name</th><td>Modify</td><td>Delete</td></tr>';
+    dataTable += '<tr><th>Vehicle Name</th><td>View Page</td><td>Modify</td><td>Delete</td></tr>';
     dataTable += '</thead>';
     dataTable += '<tbody>';
     data.forEach(function (element) {
         dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`;
+        dataTable += `<td><a target="_blank" class="crud-button" href='/inv/detail/${element.inv_id}' title='Click to view'>View</a></td>`;
         dataTable += `<td><a class="crud-button" href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`;
         dataTable += `<td><a class="crud-button" href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`;
     })
